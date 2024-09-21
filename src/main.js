dabc24962c70f98f59e6658aa9be34b57ad0f8f1
Clyde Kertzer <112921784+Clyde-Kertzer@users.noreply.github.com>
2024-09-21 10:44:34 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -28,6 +28,19 @@ function to_string_prime_factors(factors) {
 }
 
 function get_prime_factors(num) {
-    //TODO
-    return [1,2,3,4]; // example
+    const factors = [];
+    let divisor = 2;
+    
+    while (n >= 2) {if (n % divisor == 0) {
+        factors.push(divisor);
+        n = n / divisor;} 
+        else {
+            divisor++;
+        }
+  }
+  return factors;
+}
+
+const randomNumber = Math.floor(Math.random() * 10000);
+console.log('Prime factors of', randomNumber + ':', primeFactors(randomNumber).join(' '))
 }
