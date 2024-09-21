9d11b4a98edaa3974d7bc6f4b02dd61a040eda60
Clyde Kertzer <112921784+Clyde-Kertzer@users.noreply.github.com>
2024-09-21 11:49:19 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -28,7 +28,26 @@ function to_string_prime_factors(factors) {
 }
 
 function get_prime_factors(num) {
-    const factors = [];
-    factors.push(9);
-    return factors;
+    // const factors = [];
+   //  factors.push(9);
+    // return factors;
+    const primeArray = [];
+  let isPrime;
+
+  // Find divisors starting with 2
+  for (let i = 2; i <= integer; i++) {
+    if (integer % i !== 0) continue;
+
+    // Check if the divisor is a prime number
+    for (let j = 2; j <= i / 2; j++) {
+      isPrime = i % j !== 0;
+    }
+
+    if (!isPrime) continue;
+    // if the divisor is prime, divide integer with the number and store it in the array
+    integer /= i
+    primeArray.push(i);
+  }
+
+  return primeArray;
 }
