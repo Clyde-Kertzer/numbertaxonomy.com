12c7fe850113ee0348345799b0bd5b6105c0dc36
Clyde Kertzer <112921784+Clyde-Kertzer@users.noreply.github.com>
2024-10-11 14:12:04 -0600

    Update NumberEngine.js

--- src/NumberEngine.js
+++ src/NumberEngine.js
@@ -2,7 +2,7 @@ class NumberEngine {
 
     get_prime_factors(num) {
         const factors = [];
-        for (let i = 1; i <= Math.floor(Math.sqrt(num)); i++) {
+        for (let i = 1; i <= num/2; i++) {
             if (num % i == 0) {
                 factors.push(i);
             }
