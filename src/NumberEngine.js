3ff56b03aa381dd75cdf29979760b8470e36b01c
Clyde Kertzer <112921784+Clyde-Kertzer@users.noreply.github.com>
2024-10-11 14:18:44 -0600

    Update NumberEngine.js

--- src/NumberEngine.js
+++ src/NumberEngine.js
@@ -10,6 +10,14 @@ class NumberEngine {
         return factors;
     }
 
+    sum_factors(num) {
+        let sum = 0
+        for (let i of get_prime_factors(num) {
+            sum += i
+        }
+        return sum
+    }
+
     get_parity(num) { //returns string
         //TODO 
     }
